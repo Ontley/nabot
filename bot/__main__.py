30b65e144e6a523f835b2fea48405b54a564a224
Ontley <theontley@gmail.com>
2020-06-18 13:58:51 +0200

    moved time to utils

--- bot/__main__.py
+++ bot/__main__.py
@@ -4,18 +4,26 @@ from random import choice
 import json
 import asyncio
 from re import findall, match
-import os
+from os import getcwd, listdir
+from os.path import isdir
 
 
 client = commands.Bot(command_prefix = '++')
 client.remove_command('help')
 
-extensions = []
-for extension in os.listdir("./bot/Cogs"):
-    if not extension.startswith('__') and extensions != 'BotConstants.py':
-        extensions.append(extension.rstrip('.py'))
+def get_extensions(directory):
+    extensions = []
+    for entry in listdir(directory):
+        if not entry.startswith('__'):
+            entry_path = f'{directory}/{entry}'
+            if isdir(entry_path):
+                extensions += get_extensions(entry_path)
+            else:
+                extensions.append(entry.rstrip('.py'))
+    return extensions
 
-for extension in extensions:
-    client.load_extension(f'bot.Cogs.{extension}')
+print(get_extensions('./bot/Cogs'))
+# for extension in extensions:
+#     client.load_extension(f'bot.Cogs.{extension}')
 
-client.run('NzE1MTg0MjQ2NzYzMDI4NTky.Xs5n6w.f_DAY0zPVW1sHKeOx7PoqpVguAE')
+# client.run('NzE1MTg0MjQ2NzYzMDI4NTky.Xs5n6w.f_DAY0zPVW1sHKeOx7PoqpVguAE')
