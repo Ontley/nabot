ae78fd864b7523a5755923ea544e76292bcf4c4c
Ontley <theontley@gmail.com>
2020-06-18 13:36:45 +0200

    made time module

--- bot/Cogs/Moderation/AdminUtils.py
+++ bot/Cogs/Moderation/AdminUtils.py
@@ -3,37 +3,7 @@ from discord.ext import commands
 from re import match
 import asyncio
 from bot.constants import highest_admin_role_id, mute_role_id, admin_roles
-
-
-def get_total_time(times):
-    total = 0
-    for time in times:
-        try:
-            total += int(time)
-        except ValueError:
-            try:
-                time_value = int(time[:-1])
-                time_multi = {'s': 1, 'm': 60, 'h': 3600, 'd': 86400}[time[-1]]
-                total += time_value * time_multi
-            except KeyError:
-                print('Mute dicitonary key error, skipping current time request')
-    return total
-
-
-async def shorten_time(seconds):
-    minutes, seconds = divmod(seconds, 60)
-    hours, minutes = divmod(minutes, 60)
-    days, hours = divmod(hours, 24)
-    time_str = ''
-    if days:
-        time_str += f'{days} days '
-    if hours:
-        time_str += f'{hours} hours '
-    if minutes:
-        time_str += f'{minutes} minutes '
-    if seconds:
-        time_str += f'{seconds} seconds '
-    return time_str
+from bot.Cogs.Moderation import time as t
 
 
 class AdminUtils(commands.Cog):
@@ -56,7 +26,7 @@ class AdminUtils(commands.Cog):
             else:
                 #reason = ' '.join(args[i: ])
                 break
-        duration = get_total_time(times)
+        duration = t.get_total_time(times)
 
         print(user_ids)
         users = []
---
+++ bot/Cogs/Moderation/time.py
@@ -0,0 +1,31 @@
+import datetime
+
+def get_total_time(times):
+    total = 0
+    for time in times:
+        try:
+            total += int(time)
+        except ValueError:
+            try:
+                time_value = int(time[:-1])
+                time_multi = {'s': 1, 'm': 60, 'h': 3600, 'd': 86400}[time[-1]]
+                total += time_value * time_multi
+            except KeyError:
+                print('Mute dicitonary key error, skipping current time request')
+    return total
+
+
+async def shorten_time(seconds):
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    time_str = ''
+    if days:
+        time_str += f'{days} days '
+    if hours:
+        time_str += f'{hours} hours '
+    if minutes:
+        time_str += f'{minutes} minutes '
+    if seconds:
+        time_str += f'{seconds} seconds '
+    return time_str
