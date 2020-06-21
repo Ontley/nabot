53cec8e072cbb6235810d00e6c7465763f01a06c
Ontley <theontley@gmail.com>
2020-06-21 16:03:38 +0200

    changed AdminUtils to 'infractions' for newer general cogs

---
+++ bot/Cogs/Moderation/infractions.py
@@ -0,0 +1,75 @@
+import discord
+from discord.ext import commands
+from re import match
+import asyncio
+from bot.constants import mute_role_id, admin_roles
+from bot.Cogs.utils import time
+
+
+async def reapply_infractions(user_id):
+
+        # database stuff
+
+        # if active_infractions:
+        #     pass
+
+        pass # remove
+
+
+class Infractions(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    @commands.has_any_role(*admin_roles)
+    async def mute(self, ctx, *args):
+        user_ids = []
+        times = []
+        for i in range(len(args)):
+            arg = args[i].strip('.,:\'\\<>[]\{\}()')
+            if match(r'\d+[smhd]{1}', arg):
+                times.append(arg)
+
+            elif match(r'\d+', arg):
+                user_ids.append(int(arg))
+
+            else:
+                #reason = ' '.join(args[i: ])
+                break
+        duration = time.get_total_time(times)
+
+        print(user_ids)
+        users = []
+        user_names = []
+        for user_id in user_ids:
+            user = ctx.guild.get_member(user_id)
+            print(user)
+            users.append(user)
+            user_names.append(user.display_name)
+        user_names = ', '.join(user_names)
+
+        mute_role = ctx.guild.get_role(mute_role_id)
+        for user in users:
+            await user.add_roles(mute_role)
+        if duration:
+            await asyncio.sleep(duration)
+            for user in users:
+                await user.remove_roles(mute_role)
+
+    @commands.command()
+    @commands.has_any_role(*admin_roles)
+    async def unmute(self, ctx, *user_ids):
+        mute_role = ctx.guild.get_role(mute_role_id)
+        for user_id in user_ids:
+            user = ctx.guild.get_member(int(user_id))
+            if user.top_role.id == mute_role_id:
+                await user.remove_roles(mute_role)
+            else:
+                await ctx.send(f'{user.display_name} is not muted')
+
+    @commands.Cog.listener()
+    async def on_member_join(self, ctx):
+        await reapply_infractions(ctx.author.id)
+
+def setup(client):
+    client.add_cog(Infractions(client))
