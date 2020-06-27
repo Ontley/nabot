b060621e7b3c219c7d6277f7ca1346538fe5f5fc
Ontley <theontley@gmail.com>
2020-06-27 23:04:49 +0200

    made infraction deal nicely with the db, added 5sec task for grabbing active but expired infractions

--- bot/Cogs/Moderation/infractions.py
+++ bot/Cogs/Moderation/infractions.py
@@ -10,45 +10,81 @@ from datetime import timedelta, datetime
 class Infractions(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.conn = sqlite3.connect('discord_bot.db')
+        self.c = self.conn.cursor()
 
-    @staticmethod
-    async def add_infraction(_id, guild, user, _type, duration, reason):
+    def disable_infraction(self, infraction_id):
+        self.c.execute("""UPDATE infractions
+                            SET active=0
+                            WHERE id=:id""",
+                            {
+                                'id': infraction_id
+                            })
+        self.conn.commit()
+
+    def add_infraction(self, _id, guild_id, user_id, admin_id, _type, duration, reason):
         '''Adds an infraction to the database and applies it to the user, the task disable_expired_infractions handles the timing for disabling'''
-        conn = sqlite3.connect('discord_bot.db')
-        conn.row_factory = sqlite3.Row
-        c = conn.cursor()
-        c.execute("INSERT INTO infractions VALUES (:id, :active, :g_id, :u_id, :type, :expires, :reason)",
-                {
-                    'id': _id,
-                    'active': 1,
-                    'g_id': guild.id,
-                    'u_id': user.id,
-                    'type': _type,
-                    'expires': datetime.utcnow() + timedelta(seconds=duration),
-                    'reason': reason
-                }
-            )
-        conn.commit()
+        self.c.execute("""INSERT INTO infractions(id, active , guild_id, user_id, admin, type, expires, reason)
+                            VALUES (:id, :active, :g_id, :u_id, :admin_id, :type, :expires, :reason)""",
+                            {
+                                'id': _id,
+                                'active': 1,
+                                'g_id': guild_id,
+                                'u_id': user_id,
+                                'admin_id': admin_id,
+                                'type': _type,
+                                'expires': datetime.utcnow() + timedelta(seconds=duration),
+                                'reason': reason
+                            }
+                        )
+        self.conn.commit()
 
-    @commands.command()
+    @commands.command(category='moderation', cls=utils.Command)
     @utils.is_admin()
     async def mute(self, ctx):
-        '''Mutes the users from the IDs provided'''
+        '''Mutes all the users from the list of IDs provided
+        Usage:
+        `{prefix}mute 258306791174176770 474515215128461324`'''
         mute_role_id = db_funcs.get_from_guild(ctx.guild.id, 'mute_role_id')
-        mute_role = ctx.guild.get_role(int(mute_role_id))
+        if not mute_role_id:
+            await ctx.send('Mute role not set for the sever, not executing command')
+            return
+        mute_role = ctx.guild.get_role(mute_role_id)
 
         user_ids = findall(r'\d{18}', ctx.message.content)
         duration = utils.get_total_time(ctx.message.content)
-        reason = findall(r'".+"', ctx.message.content)
-        for user_id in user_ids:
-            user = ctx.guild.get_member(user_id)
-            await user.add_roles(mute_role)
-            await self.add_infraction(ctx.message.id, ctx.guild, user_id, 'mute', duration, reason)
+        try:
+            reason = findall(r'".+"', ctx.message.content)[0]
+        except IndexError:
+            empty_reason_question = await ctx.send('No string inside quotes found, do you want to apply an infraction without a reason?')
+            admin_id = ctx.message.author.id
+            await empty_reason_question.add_reaction('✔')
+            try:
+                def check(reaction, user):
+                    return str(reaction) == '✔' and user.id == admin_id and reaction.message.id == empty_reason_question.id
+                await self.client.wait_for('reaction_add', check=check, timeout=60)
+                await empty_reason_question.clear_reactions()
+                reason = 'No reason given'
+            except asyncio.TimeoutError:
+                await ctx.send('Not adding infraction without confirmation for empty reason')
+                return
 
-    @commands.command()
+        feedback_msg = f'Users {", ".join(user_ids)} muted for {duration} seconds.'
+        if reason != 'No reason given':
+            feedback_msg += f' Reason: {reason}'
+        await ctx.send(feedback_msg)
+
+        for user_id in user_ids:
+            user = ctx.guild.get_member(int(user_id))
+            await user.add_roles(mute_role)
+            self.add_infraction(ctx.message.id, ctx.guild.id, user_id, ctx.message.author.id, 'mute', duration, reason)
+
+    @commands.command(category='moderation', cls=utils.Command)
     @utils.is_admin()
     async def unmute(self, ctx=None, guild=None, user=None):
-        '''Unmutes all of the users from the IDs provided'''
+        '''Unmutes all of the users from the list of  IDs provided
+        Usage:
+        `{prefix}unmute 258306791174176770 474515215128461324`'''
         print('got into unmute')
         # mute_role_id = db_funcs.get_from_guild(ctx.guild.id, 'mute_role_id')
         # mute_role = ctx.guild.get_role(int(mute_role_id))
@@ -67,22 +103,27 @@ class Infractions(commands.Cog):
             u_id = ctx.author.id
             duration = active_mute['duration']
             reason = f'REAPPLIED ON REJOIN: \'{active_mute["reason"]}\''
-            await self.add_infraction(ctx.message.id, g_id, u_id, 'mute', duration, reason)
+            await self.add_infraction(ctx.message.id, g_id, u_id, self.client.id, 'mute', duration, reason)
 
     @tasks.loop(seconds=5.0)
     async def disable_expired_infractions(self):
         '''Checks if any infractions have expired every 5 seconds because I don't want to break my PC by running asyncio.sleep 500 times'''
         expired_infractions = db_funcs.get_expired_infractions()
+        print(expired_infractions)
 
         if expired_infractions:
             for infraction in expired_infractions:
-                guild = self.client.get_guild(infraction['guild_id'])
+                guild = await self.client.fetch_guild(infraction['guild_id'])
                 user = guild.get_member(infraction['user_id'])
                 _type = infraction['type']
                 await self.unmute(guild=guild, user=user)
-                
-                # await disable_infraction(guild, user, _type)
-                # disable the infractions found
+
+                infraction_id = infraction['id']
+                self.disable_infraction(infraction_id)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        self.disable_expired_infractions.start() # pylint: disable=maybe-no-member
 
 
 def setup(client):
